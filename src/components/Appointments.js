964f1174d51815363933af9331028c18a9ba0810
Tpulak <73133766+Tpulak@users.noreply.github.com>
2023-04-08 18:49:05 -0400

    updated appointments

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,10 +1,16 @@
 import React from "react";
+import glasses from './img/glasses.png';
+import Button from '@mui/material/Button';
 
 const Appointments = () => {
     return ( 
         <div className="App">
-            Appointments route
-            <h2>(flex)Add a button to go to Appointments page</h2>
+            <div className="logo">
+                <img src={glasses} alt="glasses" />
+            </div>
+            <h1>APPOINTMENTS</h1>
+            <h3>To schedule an appointment, please click the following</h3>
+            <Button variant="contained" color="info">Schedule Appointment</Button>
         </div>
     );
 }
