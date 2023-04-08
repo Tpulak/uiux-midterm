da3ea75930acee47aa48374acb1d99271c2efd06
Tpulak <73133766+Tpulak@users.noreply.github.com>
2023-04-08 19:03:47 -0400

    changed button color

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './css/Appointments.css';
 import glasses from './img/glasses.png';
 import Button from '@mui/material/Button';
 
@@ -10,7 +11,7 @@ const Appointments = () => {
             </div>
             <h1>APPOINTMENTS</h1>
             <h3>To schedule an appointment, please click the following</h3>
-            <Button variant="contained" color="info">Schedule Appointment</Button>
+            <Button sx={{bgcolor : '#D27D2D', ":hover": {bgcolor: "#D27D2D"} }} variant="contained" color="info" >Schedule Appointment</Button>
         </div>
     );
 }
